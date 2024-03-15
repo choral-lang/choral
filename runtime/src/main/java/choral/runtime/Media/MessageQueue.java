a24be81ba65bae37c190ae6085658de27441d83a
thesave <Thesave86@gmail.com>
2024-03-15 15:16:25 +0100

    Update MessageQueue.java

--- runtime/src/main/java/choral/runtime/Media/MessageQueue.java
+++ runtime/src/main/java/choral/runtime/Media/MessageQueue.java
@@ -28,59 +28,28 @@ import java.util.concurrent.ExecutionException;
 public class MessageQueue {
 
 	private final LinkedList< Object > sendQueue = new LinkedList<>();
-	private CompletableFuture< Object > recvFuture = new CompletableFuture<>();
-
-	public MessageQueue() {
-	}
+	private final LinkedList< CompletableFuture< Object > > recvQueue = new LinkedList<>();
 
 	public void send( Object message ) {
 		synchronized( this ) {
-			if( recvFuture.isDone() ) {
+			if ( recvQueue.isEmpty() ){
 				sendQueue.add( message );
 			} else {
-				recvFuture.complete( message );
+				CompletableFuture< Object > future = recvQueue.removeFirst();
+				future.complete( message );
 			}
 		}
 	}
 
 	public < T > T recv() throws ExecutionException, InterruptedException {
-		CompletableFuture< Object > c;
-		synchronized( this ) {
-			c = recvFuture;
-			recvFuture = new CompletableFuture<>();
-			if( !sendQueue.isEmpty() ) {
-				recvFuture.complete( sendQueue.removeFirst() );
+		CompletableFuture< Object > future = new CompletableFuture<>();
+		synchronized ( this ){
+			if( sendQueue.isEmpty() ){
+				recvQueue.add( future );
+			} else {
+				future.complete( sendQueue.removeFirst() );
 			}
 		}
-		return (T) c.get();
+		return (T) future.get();
 	}
-
-//	public void send( Object message ) {
-//		synchronized( this ) {
-//			for( int i = 0; i < recvQueue.size(); i++ ) {
-//				if( !recvQueue.get( i ).isDone() ) {
-//					recvQueue.remove( i ).complete( message );
-//					return;
-//				}
-//			}
-//			CompletableFuture< Object > c = new CompletableFuture<>();
-//			c.complete( message );
-//			sendQueue.add( c );
-//		}
-//	}
-//
-//	public < T > T recv() throws ExecutionException, InterruptedException {
-//		CompletableFuture< Object > c;
-//		synchronized( this ) {
-//			if( sendQueue.isEmpty() ) {
-//				c = new CompletableFuture<>();
-//				recvQueue.add( c );
-//			} else {
-//				c = sendQueue.removeFirst();
-//			}
-//		}
-//		return (T) c.get();
-//	}
-
-
 }
