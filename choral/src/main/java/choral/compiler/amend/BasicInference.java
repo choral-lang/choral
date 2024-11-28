14db090305d663d085fab14de89bba6825398a89
Blesting <blesteea@gmail.com>
2024-11-28 13:14:11 +0100

    cleanup

--- choral/src/main/java/choral/compiler/amend/BasicInference.java
+++ choral/src/main/java/choral/compiler/amend/BasicInference.java
@@ -49,15 +49,26 @@ public class BasicInference {
 			for( ClassMethodDefinition method : cls.methods() ){
 				HigherMethod higherMethod = method.signature().typeAnnotation().get();
 				for( Entry<World, List<Expression>> entryset : higherMethod.worldDependencies().entrySet() ){
-					World world = entryset.getKey();
+					
+					World receiver = entryset.getKey();
+					
 					for( Expression dependency : entryset.getValue() ){
-						// TODO probably assert that fromWorld can only be a single world
-						List<? extends World> fromWorld = ((GroundDataType)dependency.typeAnnotation().get()).worldArguments();
+						// TODO probably assert that sender can only be a single world
+						List<? extends World> sender = ((GroundDataType)dependency.typeAnnotation().get()).worldArguments();
 						GroundDataType dependencyType = ((GroundDataType)dependency.typeAnnotation().get()); // 99% certain this cannot be void, maybe add an assert?
 						
-						System.out.println( "Role " + world + " needs " + dependency + " of type " + dependencyType + " from role " + fromWorld );
+						System.out.println( "Role " + receiver + " needs " + dependency + " of type " + dependencyType + " from role " + sender );
 						
-						// TODO make it stop when first usable com-method is found
+						HigherMethod comMethod = findComMethod(receiver, (World)sender.get(0), higherMethod.channels());
+						if( comMethod == null ){
+							// TODO throw some exception
+							return;
+						}
+
+						Expression newExpression = createComExpression(dependency, comMethod);
+						
+						// TODO insert newExpression into the ast
+
 						for( GroundInterface channel : higherMethod.channels() ){
 							if( channel.typeArguments().size() == 1 ){ 		// checks that this channel is not a purely selection chanel (only
 																			// data channels have a type argument)
@@ -65,41 +76,6 @@ public class BasicInference {
 								System.out.println( "type argument check: " + channel.typeArguments().get(0).isSubtypeOf_relaxed(dependencyType) ); 
 								// TODO find proper way to check if channel dependencyType is a subtype of channel.typeArguments 
 								
-								Optional<? extends HigherMethod> comMethodOptional = channel.methods().filter( innerMethod -> 
-									innerMethod.identifier().equals("com") && // it is a com method (only checked through name)
-									innerMethod.innerCallable().signature().parameters().get(0).type().worldArguments().equals(fromWorld) && // its parameter's worlds are equal to our dependency's world(s)
-									innerMethod.innerCallable().returnType() instanceof GroundDataType && // probably redundant check, returntype should not be able to be void
-									((GroundDataType)innerMethod.innerCallable().returnType()).worldArguments().get(0).equals(world) ) // its returntype's world is equal to our dependency recipient
-									.findAny();
-
-								if( comMethodOptional.isPresent() ){
-									HigherMethod comMethod = comMethodOptional.get();
-									
-									/*
-									 * To make an expression we need
-									 * 	1. a name
-									 * 		(the name of out communication method (com))
-									 * 	2. argumetns
-									 * 		(our dependency expression)
-									 * 	3. type argumetns
-									 * 		com methods always need the type of the data they are communicating. 
-									 * 		this is stored as a type expression. from looking at how choral treats 
-									 * 		com methods in the examples, these type expressions onle have a name (and 
-									 * 		NOTHING else). also this name is only the simple name for the type? e.g. 
-									 * 		not "java.lang.Object", only "Object". so this is the unqualified(?) name 
-									 * 		for the type.
-									 */
-									final List<Expression> arguments = List.of(dependency);
-									final Name name = new Name(comMethod.identifier());
-									final List<TypeExpression> typeArguments = List.of(new TypeExpression(new Name(dependencyType.typeConstructor().toString()), null, null));
-									// TODO how do I get the type's identifier without relying on toString?
-									MethodCallExpression newExpression = new MethodCallExpression(name, arguments, typeArguments);
-									System.out.println( "newExpression: " + newExpression );
-									System.out.println( "MethodCallExpression: " + newExpression.toString() );
-									System.out.println( "typearguments: " + newExpression.typeArguments().get(0).name() );
-									System.out.println( "typearguments: " + newExpression.typeArguments().get(0).typeArguments() );
-									System.out.println( "typearguments: " + newExpression.typeArguments().get(0).worldArguments() );
-								}
 							}
 							
 						}
@@ -109,4 +85,53 @@ public class BasicInference {
 
 		}
 	}
+
+	private static HigherMethod findComMethod(World recepient, World sender, List<GroundInterface> channels){
+		for( GroundInterface channel : channels ){
+			Optional<? extends HigherMethod> comMethodOptional = 
+				channel.methods()
+					.filter( method ->
+						method.identifier().equals("com") && // it is a com method (only checked through name)
+						method.innerCallable().signature().parameters().get(0).type().worldArguments().equals(List.of(sender)) && // its parameter's worlds are equal to our dependency's world(s)
+						method.innerCallable().returnType() instanceof GroundDataType && // probably redundant check, returntype should not be able to be void
+						((GroundDataType)method.innerCallable().returnType()).worldArguments().get(0).equals(recepient) ) // its returntype's world is equal to our dependency recipient
+					.findAny();
+			
+			if( comMethodOptional.isPresent() ){
+				return comMethodOptional.get();
+			}
+		}
+		return null;
+	}
+
+	private static Expression createComExpression(Expression dependency, HigherMethod comMethod){
+		/*
+		* To make an expression we need
+		* 	1. a name
+		* 		(the name of out communication method (com))
+		* 	2. argumetns
+		* 		(our dependency expression)
+		* 	3. type argumetns
+		* 		com methods always need the type of the data they are communicating. 
+		* 		this is stored as a type expression. from looking at how choral treats 
+		* 		com methods in the examples, these type expressions onle have a name (and 
+		* 		NOTHING else). also this name is only the simple name for the type? e.g. 
+		* 		not "java.lang.Object", only "Object". so this is the unqualified(?) name 
+		* 		for the type.
+		*/
+		GroundDataType dependencyType = ((GroundDataType)dependency.typeAnnotation().get()); // 99% certain this cannot be void, maybe add an assert?
+		final List<Expression> arguments = List.of(dependency);
+		final Name name = new Name(comMethod.identifier());
+		final List<TypeExpression> typeArguments = List.of(new TypeExpression(new Name(dependencyType.typeConstructor().toString()), null, null));
+		// TODO how do I get the type's identifier without relying on toString?
+		MethodCallExpression newExpression = new MethodCallExpression(name, arguments, typeArguments);
+		System.out.println( "newExpression: " + newExpression );
+		System.out.println( "MethodCallExpression: " + newExpression.toString() );
+		System.out.println( "typearguments: " + newExpression.typeArguments().get(0).name() );
+		System.out.println( "typearguments: " + newExpression.typeArguments().get(0).typeArguments() );
+		System.out.println( "typearguments: " + newExpression.typeArguments().get(0).worldArguments() );
+		
+		return newExpression;
+	}
+
 }
