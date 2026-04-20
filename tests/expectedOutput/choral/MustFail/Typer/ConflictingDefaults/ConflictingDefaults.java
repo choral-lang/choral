f96321c76d17d3c19cd97a216bfe69bb38e9ddc3
Dan Plyukhin <dplyukhin@gmail.com>
2026-04-19 21:08:55 -0500

    Added back missing expected Java files

--- tests/expectedOutput/choral/MustFail/Typer/ConflictingDefaults/ConflictingDefaults.java
+++
@@ -1,7 +0,0 @@
-package choral.MustFail.Typer.ConflictingDefaults;
-
-import choral.annotations.Choreography;
-
-@Choreography( role = "A", name = "ConflictingDefaults" )
-class ConflictingDefaults implements Left,Right {
-}
